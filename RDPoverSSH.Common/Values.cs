fe6245930171cd619f755ca103ec399461e482ea
Micah Morrison <micahmo@outlook.com>
2021-12-11 21:57:07 -0500

    Fix auto-update paths

--- RDPoverSSH.Common/Values.cs
+++ RDPoverSSH.Common/Values.cs
@@ -16,7 +16,7 @@ namespace RDPoverSSH.Common
         public static readonly string SshdConfigFile = Path.Combine(SshProgramDataPath, "sshd_config");
         public static readonly string ApplicationDataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonDocuments), "RDPoverSSH");
         public static readonly string RoamingApplicationDataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "RDPoverSSH");
-        public static readonly string VersionInfoUrl = "https://raw.githubusercontent.com/micahmo/RDPoverSSH/master/RDPoverSSH/VersionInfo.xml";
+        public static readonly string VersionInfoUrl = "https://raw.githubusercontent.com/micahmo/RDPoverSSH/main/RDPoverSSH/VersionInfo.xml";
 
         public static string ClientServerPrivateKeyFilePath(int connectionId) => Path.Combine(SshProgramDataPath, $"ssh_rdp_over_ssh_{connectionId}_key");
         
